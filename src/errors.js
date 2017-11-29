0e24e2b0471ebd9723fa803060ee0a02f0a658f2
rgrannell1 <r.grannell2@gmail.com>
2017-11-29 23:04:17 +0000

    deprecate now works

--- src/errors.js
+++ src/errors.js
@@ -2,6 +2,16 @@
 const errors =  {}
 let codeCounter = 0
 
+/**
+ * @param {object} config
+ * @param {object} config.name the name of the error-subclass
+ * @param {object} config.prefix the prefix of each error-code
+ * @param {object} config.code the number of the error code
+ *
+ * Create a new error subclass.
+ *
+ * @returns {undefined}
+ */
 errors.create = config => {
   const code = config.hasOwnProperty('code')
     ? config.code
@@ -30,12 +40,22 @@ errors.create = config => {
   }
 }
 
+/**
+ * @param {function} fn the function to wrap
+ * @param {string} message the message to display
+ *
+ * Create a new error subclass.
+ *
+ * @returns {function}
+ */
+
 errors.deprecate = (fn, message) => {
   var warned = false
-  function deprecated () {
+  function deprecated (...args) {
     if (!warned) {
       console.warn(message)
     }
+    fn(...args)
   }
 
   return deprecated
