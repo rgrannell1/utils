94dd9e5eefa228cb5fa43dd08b69535765f5a8af
Ryan Grannell <rgrannell1@users.noreply.github.com>
2017-12-07 00:57:04 +0000

    Update digital-ocean.js

--- src/digital-ocean.js
+++ src/digital-ocean.js
@@ -261,43 +261,43 @@ function DigitalOcean (token) {
 Object.assign(DigitalOcean.prototype, {
   listSSHKeys () {
     return api.listSSHKeys()
-  }
+  },
   findSSHKeys (fields) {
     return api.findSSHKeys(fields)
-  }
+  },
   newSSHKey (name, publicKey) {
     return api.newSSHKey(name, public_key)
-  }
+  },
   updateSSHKey (name, publicKey) {
     return api.updateSSHKey(name, publicKey)
-  }
+  },
   setSSHKey (name, publicKey) {
     return api.setSSHKey(name, publicKey)
-  }
+  },
   listDomainRecords (domain) {
     return api.listDomainRecords(domain)
-  }
+  },
   findDomainRecord (domain, fields) {
     return api.findDomainRecord(domain, fields)
-  }
+  },
   newDomainRecord (conf) {
     return api.newDomainRecord(conf)
-  }
+  },
   removeDomainRecord (domain, subDomain) {
     return api.removeDomainRecord(domain, subDomain)
-  }
+  },
   setDomainRecord (conf) {
     return api.setDomainRecord(conf)
-  }
+  },
   listVMs () {
     return api.listVMs()
-  }
+  },
   findVMs (fields) {
     return api.findVMs(fields)
-  }
+  },
   newVm (conf) {
     return api.newVm(conf)
-  }
+  },
   setVM (conf, {sshKeyPath, sshKeyName, vmName}) {
     return api.setVM(conf, {sshKeyPath, sshKeyName, vmName})
   }
