8de98b847c699974381e4717cfd7cad4dafc4a11
rgrannell1 <r.grannell2@gmail.com>
2017-12-31 00:59:11 +0000

    added set ops

--- src/set.js
+++ src/set.js
@@ -14,4 +14,36 @@ set.equals = (set0, set1) => {
   }
 }
 
+set.union = (set0, set1) => {
+  const conjunction = new Set([])
+
+  for (let elem of set0) {
+    conjunction.add(elem)
+  }
+
+  for (let elem of set1) {
+    conjunction.add(elem)
+  }
+
+  return conjunction
+}
+
+set.intersection = (set0, set1) => {
+  const shared = new Set([])
+
+  for (let elem of set0) {
+    if (set1.has(elem) {
+      shared.add(elem)
+    }
+  }
+
+  for (let elem of set1) {
+    if (set0.has(elem) {
+      shared.add(elem)
+    }
+  }
+
+  return shared
+}
+
 module.exports = set
