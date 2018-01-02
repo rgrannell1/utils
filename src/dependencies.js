e61d553b502b0734eacff60479b5d8dc357bcdd7
rgrannell1 <r.grannell2@gmail.com>
2018-01-02 19:37:03 +0000

    added http response dependency check

--- src/dependencies.js
+++ src/dependencies.js
@@ -2,6 +2,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 const object = require('./object')
+const request = require('request')
 
 const reportState = {
   failed: Symbol('failed'),
@@ -74,7 +75,7 @@ deps.Path = class extends Dependency {
     })
   }
   [symbols.inspect] () {
-    Object.assertProperties(this, ['path'])
+    Object.assertProperties(this, ['path', 'label'])
 
     return new Promise((resolve, reject) => {
       fs.stat(this.path, err => {
@@ -98,16 +99,20 @@ deps.EnvVar = class extends Dependency {
     })
   }
   [symbols.inspect] () {
-    Object.assertProperties(this, ['variable'])
+    Object.assertProperties(this, ['variable', 'label'])
 
     return new Promise((resolve, reject) => {
+      const varExists = process.env.hasOwnProperty(this.variable)
       const ctx = {
         variable: this.variable,
-        label: this.label
+        label: this.label,
       }
-      process.env.hasOwnProperty(this.variable)
-        ? resolve({status: reportState.passed, ctx})
-        : resolve({status: reportState.failed, ctx})
+
+      const reason = varExists ? 'variable found.' : 'variable not found.'
+
+      varExists
+        ? resolve({status: reportState.passed, ctx, reason})
+        : resolve({status: reportState.failed, ctx, reason})
 
     })
   }
@@ -121,25 +126,85 @@ deps.Droplet = class extends Dependency {
     })
   }
   [symbols.inspect] () {
-    Object.assertProperties(this, ['name'])
+    Object.assertProperties(this, ['name', 'label'])
 
-    return new Promise((resolve, reject) => {
-      const ctx = {
-        variable: this.variable
-      }
-      return digitalOcean.findVMs({name: this.name}).then(vm => {
+    const ctx = {
+      variable: this.variable
+    }
+    return digitalOcean.findVMs({name: this.name})
+      .then(vm => {
         const ctx = {
           name: this.name,
           label: this.label
         }
+        const reason = vm ? 'vm exists' : 'vm does not exist.'
+
         return vm
-          ? {status: reportState.passed, ctx}
-          : {status: reportState.failed, ctx}
+          ? {status: reportState.passed, ctx, reason}
+          : {status: reportState.failed, ctx, reason}
+      })
+      .catch(err => {
+        return {status: reportState.failed, ctx, reason: 'an error occurred'}
+      })
+  }
+}
+
+deps.HttpResponse = class extends Dependency {
+  constructor (...args) {
+    super(...args)
+    Object.assign(this, {
+      label: 'http-response'
+    })
+  }
+  [symbols.inspect] () {
+    Object.assertProperties(this, ['url', 'label'])
+
+    const ctx = {
+      url: this.url,
+      label: this.label
+    }
+
+    const expectedCode = this.statusCode || 200
+
+    return new Promise((resolve, reject) => {
+      const opts = {
+        uri: this.url,
+        headers: {
+
+        }
+      }
+
+      request(opts, (err, res, body) => {
+
+        ctx.hasResponse = !!res
+        ctx.responseCode = res ? res.statusCode : null
+
+        const status = {ctx}
+
+        if (err) {
+          ctx.err = err
+        }
+
+        if (err || !res || res.statusCode !== expectedCode) {
+          status.status = reportState.failed
+
+          if (err) {
+            status.reason = 'an error occurred'
+          } else if (!res) {
+            status.reason = 'no response received'
+          } else if (res && res.statusCode !== expectedCode) {
+            status.reason = 'invalid status-code received'
+          }
+
+          resolve(status)
+        } else {
+          status.status = reportState.passed
+          status.reason = 'expected response code received'
+          resolve(status)
+        }
       })
     })
   }
 }
-deps.check([
-  new deps.EnvVar({variable: 'xxxxxx'})
-])
+
 module.exports = deps
