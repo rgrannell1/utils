abcfcbe6e92040aa446b210a5874be1f5f1984d8
rgrannell1 <r.grannell2@gmail.com>
2018-07-04 22:26:21 +0100

    improved lint command

--- build/commands/lint.js
+++ build/commands/lint.js
@@ -1,5 +1,6 @@
 
 const standard = require('standard')
+const chalk = require('chalk')
 
 const command = {
   name: 'lint',
@@ -11,12 +12,31 @@ Usage:
   script lint
 `
 
+const summariseLintErrors = results => {
+  return results.results.map(result => {
+    let message = `${chalk.inverse(result.filePath)}\n`
+
+    message += result.messages.map(({ruleId, message, source, line, column}) => {
+      const prefix = chalk.bold(`[${ruleId} ${line}:${column}]`)
+      return `  ${prefix} ${chalk.white(message)}`
+    }).join('\n')
+
+    return message
+  }).join('\n\n') + '\n\n'
+}
+
 command.task = () => {
-  const target = process.cwd() + './packages/**/*.js'
+  const target = process.cwd() + '/packages/**/*.js'
+
   return new Promise((resolve, reject) => {
     standard.lintFiles(target, {}, (err, res) => {
       err ? reject(err) : resolve(res)
     })
+  }).then(results => {
+    const failed = true
+    if (failed) {
+      throw new Error(summariseLintErrors(results))
+    }
   })
 }
 
