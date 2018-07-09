25b0f1a796b65f6b2874b981705cffb132ab1843
rgrannell1 <r.grannell2@gmail.com>
2018-07-10 00:29:00 +0100

    added description back to utils

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 [![Build Status](https://travis-ci.org/rgrannell1/utils.svg?branch=master)](https://travis-ci.org/rgrannell1/utils)
 
-
+A monorepository for publishing utility modules
 
 ## Packages
 
--- build/commands/document.js
+++ build/commands/document.js
@@ -1,9 +1,7 @@
 
 const documentation = require('documentation')
 const fs = require('fs').promises
-const md = require('@rgrannell/markdown')
 const oUtils = require('@rgrannell/object')
-const build = require('../commands')
 const mustache = require('mustache')
 
 const path = require('path')
@@ -35,27 +33,6 @@ const generatePackageDocs = async path => {
     })
   }))
 }
-
-const license = `
-Copyright (c) ${(new Date()).getFullYear()} Ryan Grannell
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.`
 
 const document = {}
 
@@ -89,7 +66,8 @@ document.utils = async args => {
 
   const template = await fs.readFile(constants.paths.utilsReadmeTemplate)
   const vars = Object.assign({}, oUtils.restrict(rootPackage, [
-    'version'
+    'version',
+    'description'
   ]))
 
   vars.year = (new Date()).getFullYear()
