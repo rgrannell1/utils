ded934b0af6c12be0ea7e5219075dc05900fa264
rgrannell1 <r.grannell2@gmail.com>
2018-07-17 00:31:20 +0100

    added errors module

---
+++ packages/errors/index.js
@@ -0,0 +1,23 @@
+
+const state = {}
+
+const CustomError = function (name, message, code, stackFrame) {
+  Object.assign(this, {name, message, code})
+  Error.captureStackTrace(this, stackFrame)
+}
+
+/**
+ * Easily construct custom-errors
+ *
+ * @example
+ *   errors.notFoundError('user-account was not found', 'ENOTFOUND')
+ */
+const errors = new Proxy(state, {
+  get: function stackFrame (_, prop) {
+    return function (message, code) {
+      return new CustomError(prop, message, code, stackFrame)
+    }
+  }
+})
+
+module.exports = errors
