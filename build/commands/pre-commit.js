94a90232b0f6b1522036c017b477cc97e2dbe1bd
rgrannell1 <r.grannell2@gmail.com>
2018-07-18 21:04:30 +0100

    test commit

--- build/commands/pre-commit.js
+++ build/commands/pre-commit.js
@@ -12,6 +12,14 @@ Description:
   Run precommit checks against this repository.
 `
 
-command.task = async args => { }
+command.task = async args => {
+  console.log(process.env.GIT_PARAMS)
+  console.log(process.env.GIT_PARAMS)
+  console.log(process.env.GIT_PARAMS)
+  console.log(process.env.GIT_PARAMS)
+  console.log(process.env.GIT_PARAMS)
+  console.log(process.env.GIT_PARAMS)
+  console.log(process.env.GIT_PARAMS)
+}
 
 module.exports = command
