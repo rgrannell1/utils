8ab5d6489e57be05b9e436655816eb64f2eb9f5a
rgrannell1 <r.grannell2@gmail.com>
2018-07-18 21:24:18 +0100

    divided commits into functions

--- build/commands/pre-commit.js
+++ build/commands/pre-commit.js
@@ -15,22 +15,31 @@ Description:
   Run precommit checks against this repository.
 `
 
-command.task = async args => {
-  const name = await branch(constants.paths.root)
+const assert = {}
 
+assert.notMaster = name => {
   if (name.includes('master')) {
-    console.error('\nWill not commit directly to master line; merge in from github from a feature branch-s\n')
+    console.error('\nWill not commit directly to master; merge in from github from a feature branch-s\n')
     process.exit(1)
   }
+}
 
-  const hasPrefix = constants.allowedBranchPrefixes.some(prefix => {
+assert.validBranchName = (name, validPrefixes) => {
+  const hasPrefix = validPrefixes.some(prefix => {
     return name.startsWith(prefix)
   })
 
   if (!hasPrefix) {
-    console.error(`\nunsupported prefix on branch "${name}"; supported prefixes are ${constants.allowedBranchPrefixes.join(', ')}\n`)
+    console.error(`\nunsupported prefix on branch "${name}"; supported prefixes are ${validPrefixes.join(', ')}\n`)
     process.exit(1)
   }
+}
+
+command.task = async args => {
+  const name = await branch(constants.paths.root)
+
+  assert.notMaster(name)
+  assert.validBranchName(name, constants.allowedBranchPrefixes)
 }
 
 module.exports = command
