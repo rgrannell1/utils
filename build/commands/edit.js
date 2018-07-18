427a310aebe1ac96f707ac4decf2fe74c3731f89
rgrannell1 <r.grannell2@gmail.com>
2018-07-18 22:07:14 +0100

    seperate out commands

--- build/commands/edit.js
+++ build/commands/edit.js
@@ -19,49 +19,7 @@ Description:
 
 const commands = {}
 
-commands['Create a Project'] = async () => {
-  const prompt = inquirer.createPromptModule()
-
-  const {name} = await prompt({
-    type: 'input',
-    name: 'name',
-    message: 'Project name:'
-  })
-
-  if (name.includes(' ') || name.includes(' ')) {
-    console.error('Project name cannot contain whitespace')
-    process.exit(1)
-  }
-
-  const {description} = await prompt({
-    type: 'input',
-    name: 'description',
-    message: 'Project description:'
-  })
-
-  await fs.mkdir(path.join(constants.paths.packages, name))
-  await Promise.all([
-    fs.mkdir(path.join(constants.paths.packages, name, 'tests')),
-    fs.mkdir(path.join(constants.paths.packages, name, 'src'))
-  ])
-
-  const promises = {}
-
-  promises.index = fs.writeFile(path.join(constants.paths.packages, name, 'index.js'), 'module.exports = {}')
-  promises.packageJson = fs.writeFile(path.join(constants.paths.packages, name, 'package.json'), JSON.stringify({
-    name: `@rgrannell/${name}`,
-    description,
-    main: 'index.js',
-    license: 'MIT',
-    publishConfig: {
-      access: 'public'
-    },
-    dependencies: {}
-  }, null, 2))
-
-  await Promise.all([promises.index, promises.packageJson])
-}
-
+commands['Create a Project'] = require('./edit/createProject')
 commands['Exit CLI'] = () => {}
 
 command.task = async args => {
